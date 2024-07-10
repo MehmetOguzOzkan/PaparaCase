887c1883035f27448e82659d805d6ee1573d6a19
MehmetOguzOzkan <mehmetouz9921@gmail.com>
2024-07-10 15:56:34 +0300

    Proje dosyası ekle.

---
+++ Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+// Custom authorization attribute to check if user is authenticated
+namespace WebApiCase1.Attributes
+{
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var userId = context.HttpContext.Items["User"];
+            if (userId == null)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using WebApiCase1.Data;
+using WebApiCase1.Models;
+
+namespace WebApiCase1.Repositories
+{
+    // Implementation of IUserRepository using Entity Framework Core
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieve a user by username
+        public User GetByUsername(string username)
+        {
+            return _context.Users.SingleOrDefault(u => u.Username == username);
+        }
+
+        // Retrieve a user by ID
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        // Add a new user to the database
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        // Retrieve all users from the database
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
